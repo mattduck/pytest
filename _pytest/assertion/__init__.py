mattduck/pytest

commit 999e7c65417f1e97fc89bf66e0da4c5cd84442ec

Tidy formatting of assertion truncation
Part two of https://github.com/pytest-dev/pytest/issues/1512. Update the format
of the truncation message to help make it clear that pytest truncates the
entire assertion output when verbosity < 2.

_pytest/assertion/__init__.py
@@ -131,14 +131,21 @@ def pytest_runtest_setup(item):
             config=item.config, op=op, left=left, right=right)
         for new_expl in hook_result:
             if new_expl:
+
+                # Truncate lines if required
                 if (sum(len(p) for p in new_expl[1:]) > 80*8 and
                         item.config.option.verbose < 2 and
                         not _running_on_ci()):
                     show_max = 10
-                    truncated_lines = len(new_expl) - show_max
-                    new_expl[show_max:] = [py.builtin._totext(
-                        'Detailed information truncated (%d more lines)'
-                        ', use "-vv" to show' % truncated_lines)]
+                    truncated_count = len(new_expl) - show_max
+                    new_expl[show_max - 1] += " ..."
+                    new_expl[show_max:] = [
+                        py.builtin._totext(""),
+                        py.builtin._totext('...Full output truncated (%d more lines)'
+                                           ', use "-vv" to show' % truncated_count
+                        ),
+                    ]
+
                 new_expl = [line.replace("\n", "\\n") for line in new_expl]
                 res = py.builtin._totext("\n~").join(new_expl)
                 if item.config.getvalue("assertmode") == "rewrite":

commit 72acda12f607be4874ac2caea0f0c0a605666c5d

WIP - start restructuing truncation
The current truncation for assert explanations does not deal with long lines
properly:

- Previously if lines were too long it could display a "-n more lines"
  message.

- 999e7c65417f1e97fc89bf66e0da4c5cd84442ec introduced a bug where long lines can
  cause index errors if there are < 10 lines.

Extract the truncation logic into its own function and ensure it can deal with
long lines properly.

_pytest/assertion/__init__.py
@@ -98,12 +98,6 @@ def pytest_collection(session):
             assertstate.hook.set_session(session)
 
 
-def _running_on_ci():
-    """Check if we're currently running on a CI system."""
-    env_vars = ['CI', 'BUILD_NUMBER']
-    return any(var in os.environ for var in env_vars)
-
-
 def pytest_runtest_setup(item):
     """Setup the pytest_assertrepr_compare hook
 
@@ -131,21 +125,7 @@ def pytest_runtest_setup(item):
             config=item.config, op=op, left=left, right=right)
         for new_expl in hook_result:
             if new_expl:
-
-                # Truncate lines if required
-                if (sum(len(p) for p in new_expl[1:]) > 80*8 and
-                        item.config.option.verbose < 2 and
-                        not _running_on_ci()):
-                    show_max = 10
-                    truncated_count = len(new_expl) - show_max
-                    new_expl[show_max - 1] += " ..."
-                    new_expl[show_max:] = [
-                        py.builtin._totext(""),
-                        py.builtin._totext('...Full output truncated (%d more lines)'
-                                           ', use "-vv" to show' % truncated_count
-                        ),
-                    ]
-
+                new_expl = util.truncate_explanation(new_expl, verbose=item.config.option.verbose)
                 new_expl = [line.replace("\n", "\\n") for line in new_expl]
                 res = py.builtin._totext("\n~").join(new_expl)
                 if item.config.getvalue("assertmode") == "rewrite":

_pytest/assertion/util.py
@@ -1,5 +1,7 @@
 """Utilities for assertion debugging"""
+import copy
 import pprint
+import os
 
 import _pytest._code
 import py
@@ -298,3 +300,65 @@ def _notin_text(term, text, verbose=False):
         else:
             newdiff.append(line)
     return newdiff
+
+
+def truncate_explanation(
+        explanation,
+        verbose=False,
+        trigger_length=80 * 8,
+        max_lines=10,
+        max_line_length=500,
+        ):
+    """ Truncate explanation lines and line length if required.
+
+    Truncate the explanation if required by the line length and
+    the pytest configuration. Add a user-friendly message.
+    """
+    new_expl = copy.deepcopy(explanation)
+
+    # Check if needs truncation
+    truncation_required = (
+        (sum(len(p) for p in new_expl[1:]) > trigger_length and
+         verbose < 2 and
+         not _running_on_ci())
+    )
+    if not truncation_required:
+        return new_expl
+
+    # Truncate extra lines
+    truncated_count = max(len(new_expl) - max_lines, 0)
+    new_expl = new_expl[:max_lines]
+
+    # Truncate individual long lines
+    shortened_count = 0
+    for i, s in enumerate(new_expl):
+        if len(s) > max_line_length:
+            new_expl[i] = s[:max_line_length] + " ..."
+            shortened_count += 1
+
+    # Format a useful message
+    msg = '...Full output truncated'
+    if shortened_count and truncated_count:
+        msg += ' (%d lines shortened and %d lines hidden)' % (
+            shortened_count, truncated_count)
+    elif shortened_count:
+        msg += ' (%d lines shortened)' % shortened_count
+    elif truncated_count:
+        msg += ' (%d lines hidden)' % truncated_count
+    msg += ', use "-vv" to show'
+
+    # Append message to the output
+    if len(new_expl[-1]) <= max_line_length:
+        new_expl[-1] += " ..."
+    new_expl.extend([
+        py.builtin._totext(""),
+        py.builtin._totext(msg),
+    ])
+
+    return new_expl
+
+
+def _running_on_ci():
+    """Check if we're currently running on a CI system."""
+    env_vars = ['CI', 'BUILD_NUMBER']
+    return any(var in os.environ for var in env_vars)