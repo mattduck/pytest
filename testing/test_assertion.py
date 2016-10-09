mattduck/pytest

commit 999e7c65417f1e97fc89bf66e0da4c5cd84442ec

Tidy formatting of assertion truncation
Part two of https://github.com/pytest-dev/pytest/issues/1512. Update the format
of the truncation message to help make it clear that pytest truncates the
entire assertion output when verbosity < 2.

_pytest/assertion/__init__.py
@@ -131,14 +131,21 @@ def pytest_runtest_setup(item):
             config=item.config, op=op, left=left, right=right)
         for new_expl in hook_result:
             if new_expl:
+
+                # Truncate lines if required
                 if (sum(len(p) for p in new_expl[1:]) > 80*8 and
                         item.config.option.verbose < 2 and
                         not _running_on_ci()):
                     show_max = 10
-                    truncated_lines = len(new_expl) - show_max
-                    new_expl[show_max:] = [py.builtin._totext(
-                        'Detailed information truncated (%d more lines)'
-                        ', use "-vv" to show' % truncated_lines)]
+                    truncated_count = len(new_expl) - show_max
+                    new_expl[show_max - 1] += " ..."
+                    new_expl[show_max:] = [
+                        py.builtin._totext(""),
+                        py.builtin._totext('...Full output truncated (%d more lines)'
+                                           ', use "-vv" to show' % truncated_count
+                        ),
+                    ]
+
                 new_expl = [line.replace("\n", "\\n") for line in new_expl]
                 res = py.builtin._totext("\n~").join(new_expl)
                 if item.config.getvalue("assertmode") == "rewrite":

commit 277a087c5604b707322e5ca63366014a789289c6

WIP: Restructure truncation of assertion messages
This addresses ref https://github.com/pytest-dev/pytest/issues/1954.

The current truncation for assertion explanations does not deal with long lines
properly:

- Previously if lines were too long it would display a "-n more lines"
  message.

- 999e7c65417f1e97fc89bf66e0da4c5cd84442ec introduced a bug where long lines can
  cause index errors if there are < 10 lines.

Extract the truncation logic into its own file and ensure it can deal with
long lines properly.

_pytest/assertion/__init__.py
@@ -7,6 +7,7 @@ import sys
 
 from _pytest.assertion import util
 from _pytest.assertion import rewrite
+from _pytest.assertion import truncate
 
 
 def pytest_addoption(parser):
@@ -98,12 +99,6 @@ def pytest_collection(session):
             assertstate.hook.set_session(session)
 
 
-def _running_on_ci():
-    """Check if we're currently running on a CI system."""
-    env_vars = ['CI', 'BUILD_NUMBER']
-    return any(var in os.environ for var in env_vars)
-
-
 def pytest_runtest_setup(item):
     """Setup the pytest_assertrepr_compare hook
 
@@ -131,21 +126,7 @@ def pytest_runtest_setup(item):
             config=item.config, op=op, left=left, right=right)
         for new_expl in hook_result:
             if new_expl:
-
-                # Truncate lines if required
-                if (sum(len(p) for p in new_expl[1:]) > 80*8 and
-                        item.config.option.verbose < 2 and
-                        not _running_on_ci()):
-                    show_max = 10
-                    truncated_count = len(new_expl) - show_max
-                    new_expl[show_max - 1] += " ..."
-                    new_expl[show_max:] = [
-                        py.builtin._totext(""),
-                        py.builtin._totext('...Full output truncated (%d more lines)'
-                                           ', use "-vv" to show' % truncated_count
-                        ),
-                    ]
-
+                new_expl = truncate.truncate_if_required(new_expl, item)
                 new_expl = [line.replace("\n", "\\n") for line in new_expl]
                 res = py.builtin._totext("\n~").join(new_expl)
                 if item.config.getvalue("assertmode") == "rewrite":

_pytest/assertion/truncate.py
@@ -1,0 +1,81 @@
+""" Utilities for truncating assertion output """
+
+import os
+
+import py
+
+
+DEFAULT_TRUNCATION_LENGTH = 8 * 80
+TRUNCATION_USAGE_MSG = "use '-vv' to show"
+
+
+def truncate_if_required(explanation, item, max_length=None):
+    """
+    Truncate this assertion explanation if the given test item is eligible.
+    """
+    if _should_truncate_item(item):
+        return _truncate_explanation(explanation)
+    return explanation
+
+
+def _should_truncate_item(item):
+    """
+    Whether or not this test item is eligible for truncation.
+    """
+    verbose = item.config.option.verbose
+    return verbose < 2 and not _running_on_ci()
+
+
+def _truncate_explanation(input_lines, max_length=None):
+    """
+    Truncate given list of strings that makes up the assertion explanation.
+
+    If the total length of the explanation is greater than max_length,
+    it will be truncated at that point. The remaining lines will be replaced by
+    a usage message.
+    """
+    if max_length is None:
+        max_length = DEFAULT_TRUNCATION_LENGTH
+
+    # Check if truncation required
+    explanation_length = len("".join(input_lines))
+    if explanation_length <= max_length:
+        return input_lines
+
+    # Find point at which input length exceeds total allowed length
+    iterated_char_count = 0
+    for iterated_index, input_line in enumerate(input_lines):
+        if iterated_char_count + len(input_line) > max_length:
+            break
+        iterated_char_count += len(input_line)
+
+    # Create truncated explanation with modified final line
+    truncated_explanation = input_lines[:iterated_index]
+    final_line = input_lines[iterated_index]
+    if final_line:
+        final_line_truncate_point = iterated_char_count - max_length
+        final_line = final_line[:final_line_truncate_point] + "..."
+    truncated_explanation.append(final_line)
+
+    # Calculate count of truncated lines
+    truncated_line_count = len(input_lines) - len(truncated_explanation)
+    truncated_line_count += 1  # Account for the part-truncated final line
+
+    # Append useful message to explanation
+    msg = '...Full output truncated'
+    if truncated_line_count == 1:
+        msg += ' ({} line hidden)'.format(truncated_line_count)
+    else:
+        msg += ' ({} lines hidden)'.format(truncated_line_count)
+    msg += ", {}" .format(TRUNCATION_USAGE_MSG)
+    truncated_explanation.extend([
+        py.builtin._totext(""),
+        py.builtin._totext(msg),
+    ])
+    return truncated_explanation
+
+
+def _running_on_ci():
+    """Check if we're currently running on a CI system."""
+    env_vars = ['CI', 'BUILD_NUMBER']
+    return any(var in os.environ for var in env_vars)

_pytest/assertion/util.py
@@ -1,5 +1,7 @@
 """Utilities for assertion debugging"""
+import copy
 import pprint
+import os
 
 import _pytest._code
 import py

testing/test_assertion.py
@@ -6,6 +6,7 @@ import _pytest.assertion as plugin
 import py
 import pytest
 from _pytest.assertion import util
+from _pytest.assertion import truncate
 
 PY3 = sys.version_info >= (3, 0)
 
@@ -572,6 +573,106 @@ class TestFormatExplanation:
         assert util.format_explanation(expl) == res
 
 
+class TestTruncateExplanation:
+
+    """
+    Confirm assertion output is truncated as expected
+    """
+
+    LINES_IN_TRUNCATION_MSG = 2
+
+    def test_truncate_explanation_when_len_EQ_total_allowed_length_and_total_lines_eq1(self):
+        max_length = 200
+        expl = ["a" * max_length]
+        result = truncate._truncate_explanation(expl, max_length=max_length)
+        assert result == expl
+
+    def test_truncate_explanation_when_len_EQ_allowed_length_and_total_lines_eq15(self):
+        max_length = 1500
+        expl = ["a" * 100] * 15
+        result = truncate._truncate_explanation(expl, max_length=max_length)
+        assert result == expl
+
+    def test_truncate_explanation_when_len_GT_allowed_length_and_total_lines_eq_20(self):
+        max_length = 640
+        expl = [str(i)[0] * 100 for i in range(20)]
+        result = truncate._truncate_explanation(expl, max_length=max_length)
+        assert result != expl
+        assert len(result) == (7 + self.LINES_IN_TRUNCATION_MSG)
+        assert "Full output truncated" in result[-1]
+        assert "14 lines hidden" in result[-1]
+        last_line_before_trunc_msg = result[- self.LINES_IN_TRUNCATION_MSG -1]
+        assert last_line_before_trunc_msg.endswith("...")
+
+    def test_truncate_explanation_when_len_GT_allowed_length_and_total_lines_eq_1(self):
+        max_length = 640
+        expl = ["a" * 641]
+        result = truncate._truncate_explanation(expl, max_length=max_length)
+        assert result != expl
+        assert len(result) == (1 + self.LINES_IN_TRUNCATION_MSG)
+        assert "Full output truncated" in result[-1]
+        assert "1 line hidden" in result[-1]
+        last_line_before_trunc_msg = result[0]
+        assert last_line_before_trunc_msg.endswith("...")
+
+    def test_truncate_explanation_when_len_LT_allowed_length_and_total_lines_eq_10(self):
+        max_length = 2000
+        expl = ["a" * 100] * 10
+        result = truncate._truncate_explanation(expl, max_length=max_length)
+        assert result == expl
+
+    def test_truncate_explanation_when_len_LT_allowed_length_and_total_lines_eq_0(self):
+        max_length = 500
+        expl = []
+        result = truncate._truncate_explanation(expl, max_length=max_length)
+        assert result == expl
+
+    def test_truncate_explanation_when_len_LT_allowed_length_and_empty_strings_given(self):
+        max_length = 500
+        expl = ['' * 2000]
+        result = truncate._truncate_explanation(expl, max_length=max_length)
+        assert result == expl
+
+    def test_full_output_message_when_len_GT_allowed_length(self, monkeypatch, testdir):
+        """
+        Test against full runpytest() output.
+        """
+
+        # the truncation point is 640 chars
+        line_count = 7
+        line_len = 100
+        expected_truncated_lines = 2
+        testdir.makepyfile(r"""
+            def test_many_lines():
+                a = list([str(i)[0] * %d for i in range(%d)])
+                b = a[::2]
+                a = '\n'.join(map(str, a))
+                b = '\n'.join(map(str, b))
+                assert a == b
+        """ % (line_len, line_count))
+        monkeypatch.delenv('CI', raising=False)
+
+        result = testdir.runpytest()
+        # without -vv, truncate the message showing a few diff lines only
+        result.stdout.fnmatch_lines([
+            "*- 1*",
+            "*- 3*",
+            "*- 5*",
+            "*truncated (%d lines hidden)*use*-vv*" % expected_truncated_lines,
+        ])
+
+        result = testdir.runpytest('-vv')
+        result.stdout.fnmatch_lines([
+            "*- %d*" % 5,
+        ])
+
+        monkeypatch.setenv('CI', '1')
+        result = testdir.runpytest()
+        result.stdout.fnmatch_lines([
+            "*- %d*" % 5,
+        ])
+
+
 def test_python25_compile_issue257(testdir):
     testdir.makepyfile("""
         def test_rewritten():
@@ -628,40 +729,6 @@ def test_sequence_comparison_uses_repr(testdir):
         "*E*'x'*",
         "*E*Extra items*right*",
         "*E*'y'*",
-    ])
-
-
-def test_assert_compare_truncate_longmessage(monkeypatch, testdir):
-    testdir.makepyfile(r"""
-        def test_long():
-            a = list(range(200))
-            b = a[::2]
-            a = '\n'.join(map(str, a))
-            b = '\n'.join(map(str, b))
-            assert a == b
-    """)
-    monkeypatch.delenv('CI', raising=False)
-
-    result = testdir.runpytest()
-    # without -vv, truncate the message showing a few diff lines only
-    result.stdout.fnmatch_lines([
-        "*- 1",
-        "*- 3",
-        "*- 5",
-        "*- 7",
-        "*truncated (193 more lines)*use*-vv*",
-    ])
-
-
-    result = testdir.runpytest('-vv')
-    result.stdout.fnmatch_lines([
-        "*- 197",
-    ])
-
-    monkeypatch.setenv('CI', '1')
-    result = testdir.runpytest()
-    result.stdout.fnmatch_lines([
-        "*- 197",
     ])
 
 
@@ -883,4 +950,3 @@ def test_issue_1944(testdir):
     result = testdir.runpytest()
     result.stdout.fnmatch_lines(["*1 error*"])
     assert "AttributeError: 'Module' object has no attribute '_obj'" not in result.stdout.str()
-